6787ad25aeb2b1dbd454589c80d9607304aec81d
Adrian Apodaca macbook air <adrian@byvapps.com>
2016-11-28 00:29:02 +0100

    Start commit

---
+++ ByvMapList/Classes/ByvMapListView.swift
@@ -0,0 +1,21 @@
+//
+//  ByvMapListView.swift
+//  Pods
+//
+//  Created by Adrian Apodaca on 27/11/16.
+//
+//
+
+import UIKit
+
+class ByvMapListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ByvMapList/Classes/Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Utils.swift
+//  Pods
+//
+//  Created by Adrian Apodaca on 27/11/16.
+//
+//
+
+import Foundation
